00cc9238f60243759c42ceae8e29b0b2b1aa4b40
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-15 11:31:58 -0500

    nodemailer setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const morgan = require('morgan');
+const nodemailer = require('nodemailer');
 const port = process.env.PORT || 5000;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/sendtome', require('./routes/sendToMe'));
+
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
