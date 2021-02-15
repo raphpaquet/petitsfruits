4deddb2ab46658a551b5c04ee2bf2c9258191836
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-15 11:31:41 -0500

    added component to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Router, Route, Switch } from "react-router";
+import { createBrowserHistory } from "history";
+import Navigation from './components/Navigation';
+import Illustrations from './components/Illustrations';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <Navigation />
+        <Illustrations />
+        <About />
+        <Contact />
+      </div>
+    </Router>
   );
 }
 
