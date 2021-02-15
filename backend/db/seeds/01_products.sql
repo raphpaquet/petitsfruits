1ca9aadccd52670b4b505ff34740d534e5333951
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-15 16:36:46 -0500

    addedproducts

--- backend/db/seeds/01_products.sql
+++ backend/db/seeds/01_products.sql
@@ -1,2 +1,15 @@
 INSERT INTO products (name, description, quantity, image, price, category)
-VALUES ('Radishes', 'Bunch of radishes', 9, './docs/raddish.jpeg', 3.99, 'Vegetables');
+VALUES ('Lampe Caulfield', 'Peinture sur toile - 9x12"', 1, './docs/lamp.png', 100.00, 'peinture');
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Leave Me Alone', 'Peinture sur toile - 7x9"', 1, './docs/leaveMeAlone.png', 75.00, 'peinture');
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Vin Vilain', 'Peinture sur papier aquarelle - 9x12"', 1, './docs/vinVilain.png', 50.00, 'peinture');
+
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Jazmine', 'sticker 2x2"', 10, './docs/jazmine.webp', 3.00, 'sticker');
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Petitsfruits', 'sticker 2x2"', 15, './docs/fraise.webp', 3.00, 'sticker');
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Banlieue feu', 'sticker 2x2"', 1, './docs/banlieue_feu.webp', 3.00, 'sticker');
+INSERT INTO products (name, description, quantity, image, price, category)
+VALUES ('Banlieue feu', 'sticker 2x2"', 1, './docs/banlieue_feu2.webp', 3.00, 'sticker');
