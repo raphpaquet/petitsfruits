fe2d9e86ad540fc76deb64a4ec0cb1fe2ffceb89
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-16 16:05:41 -0500

    changed size

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -6,11 +6,14 @@ import { Link } from 'react-router-dom'
 export default function Navigation() {
   return (
     <section id="navbar">
+      
       <div className='page-title'>
         <img className="logo" src="/images/logo.png"></img>
         <span className="name">| petitsfruits</span>
       </div>
-      <img className="skate" src="./images/skate.png"></img>
+      {/* <div className="image-skate">
+        <img className="skate" src="./images/skate.png"></img>
+      </div> */}
       <div className="tab-button">
         <div className="tab-list">
           <HashLink to="#illustrations" className="action-btn">Illustrations</HashLink>
