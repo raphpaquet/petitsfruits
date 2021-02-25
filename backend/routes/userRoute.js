4c46c42219c37a9b7f221f23cf3fa021b495f38c
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-25 14:24:10 -0500

    implemented product and user route

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import User from '../models/userModels';
+import { getToken } from '../utils'
+
+const router = express.Router();
+
+
+router.post('/signin', async (req, res) => {
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  });
+  if(signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser)
+    })
+  } else {
+    res.status(404).send({msg: 'Invalid Email or password.'})
+  }
+})
+
+router.get("/createadmin", async (req, res) => {
+  try {
+    const user = new User({
+      name: 'Sarah',
+      email: 'sarahpaquet@gmail.com',
+      password: 'test',
+      isAdmin: true
+    });
+  
+    const newUser = await user.save();
+    res.send(newUser); 
+  } catch (error) {
+    res.send({ msg: error.message });
+  }
+})
+
+export default router;
