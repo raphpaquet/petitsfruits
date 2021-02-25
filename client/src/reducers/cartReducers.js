dcb36162e8988250367d880a45f9f16702b6b275
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-25 14:22:02 -0500

    updated reducers

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,20 +1,20 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
-function cartReducer(state= {cartItem: []}, action) {
+function cartReducer(state= { cartItems: [] }, action) {
   switch(action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-      const product = state.cartItem.find(x => x.product === item.product);
+      const product = state.cartItems.find(x => x.product === item.product);
       if(product) {
         return { cartItems: 
-          state.cartItem.map(x => x.product === product.product ? item : x)
+          state.cartItems.map(x => x.product === product.product ? item : x)
         };
       }
       return { cartItems: [...state.cartItems, item] };
     
     case CART_REMOVE_ITEM: 
       return { cartItems: 
-        state.cartItems.filter(x => x.product !== action.payload)}
+        state.cartItems.filter(x => x.product !== action.payload) }
     default: 
       return state;
   }
--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -1,4 +1,17 @@
-import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { 
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DETAILS_FAIL, 
+  PRODUCT_DETAILS_REQUEST, 
+  PRODUCT_DETAILS_SUCCESS, 
+  PRODUCT_LIST_FAIL, 
+  PRODUCT_LIST_REQUEST, 
+  PRODUCT_LIST_SUCCESS, 
+  PRODUCT_SAVE_FAIL, 
+  PRODUCT_SAVE_REQUEST, 
+  PRODUCT_SAVE_SUCCESS 
+} from "../constants/productConstants";
 
 
 
@@ -18,7 +31,7 @@ function productListReducer(state= {products: []}, action) {
   }
 }
 
-function productDetailsReducer(state= {product: {} }, action) {
+function productDetailsReducer(state= { product: {} }, action) {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return {loading: true}
@@ -33,4 +46,38 @@ function productDetailsReducer(state= {product: {} }, action) {
       return state;
   }
 }
-export { productListReducer, productDetailsReducer };
+
+function productSaveReducer(state= { product: {} }, action) {
+  switch (action.type) {
+    case PRODUCT_SAVE_REQUEST:
+      return {loading: true}
+    
+    case PRODUCT_SAVE_SUCCESS:
+      return {loading:false, success: true, product: action.payload};
+    
+    case PRODUCT_SAVE_FAIL:
+      return {loading:false, error: action.payload}
+    
+    default: 
+      return state;
+  }
+}
+
+function productDeleteReducer(state= { product: {} }, action) {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return {loading: true}
+    
+    case PRODUCT_DELETE_SUCCESS:
+      return {loading:false, success: true, product: action.payload, success:true };
+    
+    case PRODUCT_DELETE_FAIL:
+      return {loading:false, error: action.payload}
+    
+    default: 
+      return state;
+  }
+}
+
+
+export { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer };
