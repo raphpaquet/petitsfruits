9259ae8eebea45a2a2b16282e3e30e63b391ad68
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-25 14:22:54 -0500

    updated initial state

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,22 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
 import thunk from 'redux-thunk';
 import Cookie from 'js-cookie';
+import { userSigninReducer } from './reducers/userReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
+const userInfo = Cookie.getJSON("userInfo") || null;
 
 
-const initialState = { cart: {cartItems}};
+const initialState = { cart: {cartItems}, userSignIn: {userInfo} };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer, 
-  cart: cartReducer
-
+  cart: cartReducer,
+  userSignin: userSigninReducer,
+  productSave: productSaveReducer,
+  productDelete: productDeleteReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
