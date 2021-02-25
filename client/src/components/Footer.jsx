29cb2a8dff7e505d7dcc80b0f61e3f58ba54d09d
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-25 14:21:21 -0500

    added admin link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,23 @@
-import './Footer.scss'
+import './Footer.scss';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Footer() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const {userInfo} = userSignin
+
   return (
   <footer id="footer">
     <div className="rights">
       @ 2021 Raphaëlle Paquet | All Rights Reserved.
     </div>
+    {
+      userInfo ? <Link to="/admin">Admin page</Link> :
+    <div className="admin">
+      <Link to="/signin">Admin</Link>
+    </div>
+    }
   </footer>
   )
 }
