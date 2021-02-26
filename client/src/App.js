5c3160213a3c1d5de8cc59ab4f7f2ce6d20bb94a
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-25 22:50:54 -0500

    added homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import './App.scss';
 import { Router, Route, Switch, BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import Navigation from './components/Navigation';
-import Illustrations from './components/Illustrations';
-import About from './components/About';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Store from './components/Store';
 import Order from './components/Order';
@@ -15,11 +11,18 @@ import Product from './components/Product';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
 import Signin from './components/Signin';
+import { useSelector } from 'react-redux';
+import HomePage from './components/HomePage';
 
 
 const history = createBrowserHistory();
 
 function App() {
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+
   return (
     <BrowserRouter >
       <div className="App">
@@ -31,12 +34,9 @@ function App() {
           <Route path="/placeorder" component={PlaceOrder} />
           <Route path="/product/:id" component={Product} />
           <Route path="/cart/:id?" component={Cart} />
+          <Route path="/category/:id" component={Store} />
           <Route path='/signin' component={Signin} />
-          <Route path='/' exact={true}>
-            <Navigation />
-            <Illustrations />
-            <About />
-            <Contact />
+          <Route path='/' exact={true} component={HomePage}>
           </Route>
         <Footer />
       </div>
--- client/src/store.js
+++ client/src/store.js
@@ -9,7 +9,7 @@ const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
 
 
-const initialState = { cart: {cartItems}, userSignIn: {userInfo} };
+const initialState = { cart: {cartItems, shipping:{}, payment:{} }, userSignIn: {userInfo} };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer, 
